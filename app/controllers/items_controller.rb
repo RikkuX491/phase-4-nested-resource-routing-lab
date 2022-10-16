1c7e19b0e137c2243c47ab7d718bb6f3e54945ca
Ricardo Guerra <RikkuG491@gmail.com>
2022-10-16 16:30:22 -0400

    commit tree 84d076932f048ccaa2d4b613dc61888ac8c039ef

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user
+        items = user.items
+        return render json: items, include: :user
+      else
+        return render json: { error: "User Not Found" }, status: :not_found
+      end
+    else
+      items = Item.all
+      return render json: items, include: :user
+    end
+  end
+
+  def show
+    item = Item.find_by(id: params[:id])
+    if item
+      render json: item, include: :user
+    else
+      render json: { error: "Item Not Found" }, status: :not_found
+    end
+  end
+
+  def create
+    user = User.find_by(id: params[:user_id])
+    if user
+      new_item = user.items.create(name: params[:name], description: params[:description], price: params[:price])
+      render json: new_item, status: :created
+    else
+      render json: { error: "User Not Found" }, status: :not_found
+    end
   end
 
 end
